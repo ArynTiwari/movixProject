cdbb33a8c22048f1e2f45ff43109d187f3463dad
Aryan Tiwari <arutiwari500@gmail.com>
2023-07-13 18:17:08 +0530

    added persist

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,13 @@ import App from "./App.jsx";
 import "./index.scss";
 import { Provider } from "react-redux";
 import { store } from "./store/store";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
+let persistor = persistStore(store);
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <App />
+    <PersistGate persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>
 );
--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,18 @@
 import { configureStore } from "@reduxjs/toolkit";
+import storage from "redux-persist/lib/storage";
+import { persistReducer } from "redux-persist";
+import { combineReducers } from "@reduxjs/toolkit";
 import homeSlice from "./homeSlice";
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
+const combinedReducer = combineReducers({
+  home: homeSlice,
+});
+const persistedReducer = persistReducer(persistConfig, combinedReducer);
+
 export const store = configureStore({
-  reducer: {
-    home: homeSlice,
-  },
+  reducer: persistedReducer,
 });
