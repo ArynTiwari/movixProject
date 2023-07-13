2a02a6e82448e13281c97f08a613230c849e13c5
Aryan Tiwari <arutiwari500@gmail.com>
2023-07-13 21:47:37 +0530

    updated Store

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import storage from "redux-persist/lib/storage";
-import { persistReducer } from "redux-persist";
+import {
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
 import { combineReducers } from "@reduxjs/toolkit";
 import homeSlice from "./homeSlice";
 const persistConfig = {
@@ -16,4 +24,10 @@ const persistedReducer = persistReducer(persistConfig, combinedReducer);
 
 export const store = configureStore({
   reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
